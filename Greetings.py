130695005ec6e8b1cf47a4026ad0947db8076a7b
IsaiahSantiago <IsaiahSantiago456@gmail.com>
2023-10-25 19:35:08 -0400

    fixed greetings and created greet & Plural py

---
+++ Greetings.py
@@ -0,0 +1,8 @@
+def greet():
+    print("hello")
+    
+
+for i in range(0, 5, 1 ):
+    greet()
+        
+
---
+++ pluralize.py
@@ -0,0 +1,7 @@
+def pluralize(word):
+    return word + "s"
+
+
+userWord = input("Word please...")
+print(pluralize(userWord))
+
